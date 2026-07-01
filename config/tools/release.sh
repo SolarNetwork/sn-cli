01e1d52dc54ac4f999995e860c9a05f545947dbb
Matt Magoffin <matt@solarnetwork.net>
2026-07-01 16:51:13 +1200

    Add release script.

---
+++ config/tools/release.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env sh
+
+set -e
+
+SN_CLI_HOME="${SN_CLI_HOME:-$(dirname "$0")/../..}"
+DRY_RUN=""
+
+. semver.sh
+
+while getopts ":n" opt; do
+	case $opt in
+		n) DRY_RUN="1";;
+		*)
+			echo "Unknown argument -${OPTARG}"
+			exit 1
+	esac
+done
+shift $(($OPTIND - 1))
+
+version="$1"
+if [ -z "$version" ]; then
+	echo 'Pass releaes version as argument.'
+	exit 1
+fi
+
+# portable sed -i
+case $(sed --help 2>&1) in
+  *GNU*) sed_i () { sed -i "$@"; };;
+  *) sed_i () { sed -i '' "$@"; };;
+esac
+
+updateVersions () {
+	for f in gradle.properties app/src/main/resources/s10k/tool/version.properties; do
+		if [ ! -e "$f" ]; then
+			echo "ERROR: missing file $f" >&2
+			echo "Looking in SN_CLI_HOME: $SN_CLI_HOME" >&2
+			exit 1
+		elif [ -n "$DRY_RUN" ]; then
+			echo "RELEASE: Update $f version to $1"
+		else
+			sed_i -e 's/^version = .*/version = '"$1"'/' "$f"
+		fi
+	done
+}
+
+nextDevRelease () {
+    local major=0
+    local minor=0
+    local patch=0
+    local special=0
+    semverParseInto $1 major minor patch special
+    echo "$major.$minor.$(($patch + 1))-dev.0"
+}
+
+doRelease () {
+	cd "$SN_CLI_HOME"
+	if [ -n "$DRY_RUN" ]; then
+		echo "RELEASE: DRY RUN: no changes will be made."
+	fi
+
+	echo "RELEASE: start release $version"
+	if [ -z "$DRY_RUN" ]; then
+		git flow release start $version
+	fi
+	
+	updateVersions $version
+
+	echo "RELEASE: commit release version changes."
+	if [ -z "$DRY_RUN" ]; then
+		git add .
+		git commit -S -m 'Bump version for next release.'
+		git flow release finish -s
+		git switch develop
+	fi
+	
+	updateVersions "$(nextDevRelease $version)"
+	
+	
+	echo "RELEASE: commit development version changes."
+	if [ -z "$DRY_RUN" ]; then
+		git add .
+		git commit -S -m 'Bump version for next development cycle.'
+		git push --all
+		git push --tags
+	fi
+}
+
+(doRelease)
