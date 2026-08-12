a6630800fa39f2f6d8370b6e63cb5b64bc0a4fa2
Matt Magoffin <matt@solarnetwork.net>
2026-08-12 13:07:15 +1200

    Add cloud-integrations datum-streams delete command.

---
+++ c2c/src/main/java/s10k/tool/c2c/ds/cmd/DeleteDatumStreamsCmd.java
@@ -0,0 +1,115 @@
+package s10k.tool.c2c.ds.cmd;
+
+import static s10k.tool.c2c.util.CloudIntegrationRestUtils.listCloudDatumStreams;
+import static s10k.tool.common.util.RestUtils.checkSuccess;
+import static s10k.tool.common.util.TableUtils.tableConfig;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import s10k.tool.c2c.domain.CloudDatumStreamConfiguration;
+import s10k.tool.c2c.domain.CloudIntegrationsFilter;
+import s10k.tool.common.cmd.BaseSubCmd;
+import s10k.tool.common.domain.ResultDisplayMode;
+import s10k.tool.common.util.TableUtils;
+
+/**
+ * List Cloud Datum Stream configurations.
+ */
+@Component
+@Command(name = "delete", sortSynopsis = false, showDefaultValues = true, descriptionHeading = "%n", optionListHeading = "%n", description = {
+		"Delete Cloud Datum Stream entities.%n" })
+public class DeleteDatumStreamsCmd extends BaseSubCmd<DatumStreamsCmd> implements Callable<Integer> {
+
+	// @formatter:off
+	@Option(names = { "-stream", "--stream-id" },
+			description = "a datum stream ID to delete",
+			split = "\\s*,\\s*",
+			splitSynopsisLabel = ",",
+			paramLabel = "datumStreamId",
+			required = true)
+	Long[] datumStreamIds;
+
+	@Option(names = { "-mode", "--display-mode" },
+			description = "how to display the data")
+	ResultDisplayMode displayMode;
+	// @formatter:on
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param reqFactory   the HTTP request factory to use
+	 * @param objectMapper the mapper to use
+	 */
+	public DeleteDatumStreamsCmd(ClientHttpRequestFactory reqFactory, ObjectMapper objectMapper) {
+		super(reqFactory, objectMapper);
+	}
+
+	@Override
+	public Integer call() throws Exception {
+		try {
+			final RestClient restClient = restClient();
+			final ResultDisplayMode displayMode = displayMode(this.displayMode);
+			final ObjectWriter pretty = objectMapper.writerWithDefaultPrettyPrinter();
+
+			final var filter = new CloudIntegrationsFilter();
+			filter.setDatumStreamIds(List.of(datumStreamIds));
+
+			final List<CloudDatumStreamConfiguration> confs = listCloudDatumStreams(restClient, objectMapper, filter);
+			if (confs.isEmpty()) {
+				System.err.println("No datum streams matched your criteria.");
+				return 0;
+			}
+
+			if (!isDryRun()) {
+				for (CloudDatumStreamConfiguration stream : confs) {
+					deleteCloudDatumStream(restClient, objectMapper, stream.configId());
+				}
+			}
+
+			List<?> tableData = (displayMode == ResultDisplayMode.JSON ? confs
+					: confs.stream().map(c -> ListDatumStreamsCmd.tableDataRow(c, false, pretty)).toList());
+			TableUtils.renderTableData(ListDatumStreamsCmd.tableDataColumns(), tableData,
+					tableConfig(this, displayMode), objectMapper, TableUtils.TableDataJsonPrettyPrinter.INSTANCE,
+					System.out);
+			return 0;
+		} catch (Exception e) {
+			System.err.println("Error viewing cloud datum streams: %s".formatted(e.getMessage()));
+		}
+		return 1;
+	}
+
+	/**
+	 * Delete a cloud datum stream.
+	 * 
+	 * @param restClient    the REST client
+	 * @param objectMapper  the object mapper
+	 * @param datumStreamId the datum stream ID to delete
+	 * @return the result
+	 * @throws IllegalStateException if an error occurs fetching the stream
+	 */
+	public static void deleteCloudDatumStream(RestClient restClient, ObjectMapper objectMapper, Long datumStreamId) {
+		// @formatter:off
+		checkSuccess(restClient.delete()
+			.uri(b -> b.path("/solaruser/api/v1/sec/user/c2c/datum-streams/{datumStreamId}")
+				.build(datumStreamId)
+			)
+			.accept(MediaType.APPLICATION_JSON)
+			.retrieve()
+			.body(JsonNode.class)
+			);		
+		// @formatter:on
+	}
+
+}
---
+++ docs/docs/commands/cloud-integrations/datum-streams/delete.md
@@ -0,0 +1,105 @@
+---
+title: delete
+---
+# Cloud Datum Stream Delete
+
+Delete [Cloud Datum Stream][datum-stream] entities.
+
+## Usage
+
+```
+s10k cloud-integrations datum-streams delete
+	[-stream=datumStreamId[,datumStreamId...]]...
+    [-mode=<displayMode>]
+```
+
+<div markdown="1" class="options-explicit-col-widths">
+
+| Option | Long Version | Description |
+|:-------|:-------------|:------------|
+| `-stream=` | `--stream-id=` | the datum stream ID(s) to match; any ID that does not match an existing entity will be ignored |
+| `-mode=` | `--display-mode=` | the format to display the data as, one of `CSV`, `JSON`, or `PRETTY`; defaults to `PRETTY` |
+
+</div>
+
+!!! tip
+
+	Use the `--dry-run` [global option](../../../../global-options.md) to simulate what entities would be deleted,
+	without actually deleting anything. For example:
+
+	```sh
+	s10k --dry-run cloud-integrations datum-streams poll-tasks change-state --stream-id 100
+	```
+
+## Output
+
+A listing of the deleted datum streams.
+
+## Examples
+
+=== "Delete datum streams"
+
+	```sh
+	s10k cloud-integrations datum-streams delete --stream-id 9
+	```
+
+=== "Delete datum streams (shortcut)"
+
+	You can use `c2c` instead of `cloud-integrations` and `ds` instead of `datum-streams`:
+
+	```sh
+	s10k c2c ds delete --stream-id 9
+	```
+
+=== "Pretty Output"
+
+	```
+	+----+-------------------+-----------+---------+------+-----------+-------------------+----------+------------+------------------------+
+	| ID | Name              | Type      | Enabled | Kind | Object ID | Source ID         | Schedule | Mapping ID | Service Properties     |
+	+----+-------------------+-----------+---------+------+-----------+-------------------+----------+------------+------------------------+
+	|  9 | A Solar Farm Site | SolarEdge | true    | n    |       123 | site/solaredge-v1 | 900      |          4 | {                      |
+	|    |                   |           |         |      |           |                   |          |            |   "placeholders" : {   |
+	|    |                   |           |         |      |           |                   |          |            |     "siteId" : 0000000 |
+	|    |                   |           |         |      |           |                   |          |            |   }                    |
+	|    |                   |           |         |      |           |                   |          |            | }                      |
+	+----+-------------------+-----------+---------+------+-----------+-------------------+----------+------------+------------------------+
+	```
+
+=== "CSV Output"
+
+	```csv
+	ID,Name,Type,Enabled,Kind,Object ID,Source ID,Schedule,Mapping ID,Service Properties
+	9,A Solar Farm Site,SolarEdge,true,n,123,site/solaredge-v1,900,4,"{
+	""placeholders"" : {
+		""siteId"" : 0000000
+	}
+	}"
+	```
+
+=== "JSON Output"
+
+	```json
+	[
+	  {
+	    "configId": 4,
+	    "name": "A Solar Farm Site",
+	    "serviceIdentifier": "s10k.c2c.ds.solaredge.v1",
+	    "created": "2024-10-24 04:40:14.538415Z",
+	    "modified": "2024-10-24 04:40:14.538415Z",
+	    "enabled": true,
+	    "datumStreamMappingId": 4,
+	    "schedule": "900",
+	    "kind": "n",
+	    "objectId": 123,
+	    "sourceId": "site/solaredge-v1",
+	    "serviceProperties": {
+	      "placeholders": {
+	        "siteId": 0000000
+	      }
+	    }
+	  }
+	]
+	```
+
+
+[datum-stream]: https://github.com/SolarNetwork/solarnetwork/wiki/SolarUser-Cloud-Integrations-API#cloud-datum-stream
