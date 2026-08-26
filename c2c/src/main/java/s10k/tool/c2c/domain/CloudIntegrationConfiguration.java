182de9a026e4af40cb861bd77171d4ec40cbdda7
Matt Magoffin <matt@solarnetwork.net>
2026-08-27 08:41:03 +1200

    Add explicit toSetting() generator.

--- c2c/src/main/java/s10k/tool/c2c/domain/CloudIntegrationConfiguration.java
+++ c2c/src/main/java/s10k/tool/c2c/domain/CloudIntegrationConfiguration.java
@@ -1,9 +1,14 @@
 package s10k.tool.c2c.domain;
 
+import static s10k.tool.common.domain.ServiceConfiguration.SERVICE_PROPERTIES_KEY;
+
 import java.time.Instant;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+
+import net.solarnetwork.codec.JsonUtils;
 
 /**
  * Cloud Integration configuration.
@@ -11,5 +16,22 @@ import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 @RegisterReflectionForBinding
 public record CloudIntegrationConfiguration(Long configId, String name, String serviceIdentifier, Instant created,
 		Instant modified, boolean enabled, Map<String, Object> serviceProperties) {
+
+	/**
+	 * Get a mapping of this entity's settings.
+	 * 
+	 * @return the settings
+	 */
+	public Map<String, Object> toSettings() {
+		Map<String, Object> result = new LinkedHashMap<>(9);
+		result.put("name", name);
+		result.put("serviceIdentifier", serviceIdentifier);
+		result.put("enabled", enabled);
+		if (serviceProperties != null) {
+			// perform a deep copy here
+			result.put(SERVICE_PROPERTIES_KEY, JsonUtils.getStringMapFromObject(serviceProperties));
+		}
+		return result;
+	}
 
 }
